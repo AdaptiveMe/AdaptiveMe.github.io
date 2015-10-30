ffcc137009bfdd7d7b14b973e589cac9db6ad374
Carlos Lozano <carlos@adaptive.me>
2015-10-30 13:21:58 +0100

    DataTable initialisation issue.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,44 @@
 
 $(document).ready(function () {
 
-    $('#builds').DataTable({
-        "paging": false,
-        "ordering": true,
-        "info": false,
-        "searching": true,
-        "order": [[ 1, "asc" ]]
-    });
+    if ( $.fn.dataTable.isDataTable( '#table-build' ) ) {
+        $('#table-build').DataTable();
+    } else {
+        $('#table-build').DataTable({
+            "paging": false,
+            "ordering": true,
+            "info": false,
+            "searching": true,
+            "order": [[ 1, "asc" ]]
+        });
+    }
 
-    $('#projects').DataTable({
-        "paging": false,
-        "ordering": true,
-        "info": false,
-        "searching": true,
-        "order": [[ 1, "asc" ]]
-    });
+    if ( $.fn.dataTable.isDataTable( '#table-projects' ) ) {
+        $('#table-projects').DataTable();
+    } else {
+        $('#table-projects').DataTable({
+            "paging": false,
+            "ordering": true,
+            "info": false,
+            "searching": true,
+            "order": [[ 1, "asc" ]]
+        });
+    }
 
-    $('#seo').DataTable({
-        "paging": false,
-        "ordering": true,
-        "info": true,
-        "searching": true,
-        "order": [[ 0, "asc" ]]
-    });
+    if ( $.fn.dataTable.isDataTable( '#seo' ) ) {
+        $('#table-seo').DataTable();
+    } else {
+        $('#table-seo').DataTable({
+            "paging": false,
+            "ordering": true,
+            "info": true,
+            "searching": true,
+            "order": [[ 0, "asc" ]]
+        });
+    }
+
+
+
 
     $('.diagram').each(function(i, obj) {
         $(obj).sequenceDiagram({theme: 'hand'});
