ae9286e307c3a754a7f92614255d61d2e1aa9e4d
Carlos Lozano <carlos@adaptive.me>
2015-10-30 11:38:58 +0100

    Minor update.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $('#builds').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": false,
+        "searching": true,
+        "order": [[ 1, "asc" ]]
+    });
+});
+
+$(document).ready(function () {
+    $('#projects').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": false,
+        "searching": true,
+        "order": [[ 1, "asc" ]]
+    });
+
+    $('#seo').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": true,
+        "searching": true,
+        "order": [[ 0, "asc" ]]
+    });
+});
+
