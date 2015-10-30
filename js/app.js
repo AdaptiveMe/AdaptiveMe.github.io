308dc69cb0ae147af550c1da53ae3ea15537ec6a
Carlos Lozano <carlos@adaptive.me>
2015-10-30 13:37:48 +0100

    Revert.

--- js/app.js
+++ js/app.js
@@ -1,35 +1,33 @@
 
 $(document).ready(function () {
 
-    if ( ! $.fn.dataTable.isDataTable( '#table-build' ) ) {
-        $('#table-build').DataTable({
-            "paging": false,
-            "ordering": true,
-            "info": false,
-            "searching": true,
-            "order": [[ 1, "asc" ]]
-        });
-    }
 
-    if ( ! $.fn.dataTable.isDataTable( '#table-projects' ) ) {
-        $('#table-projects').DataTable({
-            "paging": false,
-            "ordering": true,
-            "info": false,
-            "searching": true,
-            "order": [[ 1, "asc" ]]
-        });
-    }
+    $('#table-build').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": false,
+        "searching": true,
+        "order": [[1, "asc"]]
+    });
 
-    if ( ! $.fn.dataTable.isDataTable( '#table-seo' ) ) {
-        $('#table-seo').DataTable({
-            "paging": false,
-            "ordering": true,
-            "info": true,
-            "searching": true,
-            "order": [[ 0, "asc" ]]
-        });
-    }
+
+    $('#table-projects').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": false,
+        "searching": true,
+        "order": [[1, "asc"]]
+    });
+
+
+    $('#table-seo').DataTable({
+        "paging": false,
+        "ordering": true,
+        "info": true,
+        "searching": true,
+        "order": [[0, "asc"]]
+    });
+
 
     $('.diagram').each(function(i, obj) {
         $(obj).sequenceDiagram({theme: 'hand'});
